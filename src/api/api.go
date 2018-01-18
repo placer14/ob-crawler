e7e4762b3503518c919f8664d99f5181cab17624
Mike Greenberg <mg@nobulb.com>
2018-01-18 13:01:07 -0500

    Extract api.doRequest and create remaining supporting endpoints

--- src/api/api.go
+++ src/api/api.go
@@ -6,6 +6,15 @@ import (
 	"net/http"
 )
 
+type OpenBazaarlikeAPI interface {
+	HostPort() string
+	GetPeers() ([]string, error)
+	GetClosestPeers(string) ([]string, error)
+	GetListingsCount(string) (int, error)
+}
+
+// Client is a configurable abstraction around the OpenBazaar API which handles
+// adding the authentication header to endpoints of interest
 type Client struct {
 	*http.Client
 	Host       string
@@ -13,37 +22,79 @@ type Client struct {
 	AuthCookie string
 }
 
+// New returns a pointer to a new Client
 func New() *Client {
 	return &Client{
 		Client: &http.Client{},
 	}
 }
 
-func (c *Client) GetPeers() ([]string, error) {
-	var (
-		err         error
-		peers       []string
-		req         *http.Request
-		requestPath string
-	)
+func (c *Client) HostPort() string { return fmt.Sprintf("%s:%d", c.Host, c.Port) }
 
-	requestPath = fmt.Sprintf("http://%s:%d/ob/%s", c.Host, c.Port, "peers")
-	req, err = http.NewRequest(http.MethodGet, requestPath, nil)
+func (c *Client) doRequest(endpoint string) (*http.Response, error) {
+	requestPath := fmt.Sprintf("http://%s:%d/ob/%s", c.Host, c.Port, endpoint)
+	req, err := http.NewRequest(http.MethodGet, requestPath, nil)
 	if err != nil {
 		return nil, fmt.Errorf("creating request: %s\n", err)
 	}
-	req.Header.Add("cookie", c.AuthCookie)
+	req.Header.Add("Cookie", c.AuthCookie)
+
 	resp, err := c.Client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("request error: %s\n", err)
+		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request error: status %s\n", resp.Status)
+		return nil, fmt.Errorf("status not ok: %s\n", resp.Status)
+	}
+	return resp, nil
+}
+
+// GetClosestPeers retrieves the list of peers of the node referenced by
+// the hash originHash
+func (c *Client) GetClosestPeers(originHash string) ([]string, error) {
+	resp, err := c.doRequest(fmt.Sprintf("closestpeers/%s", originHash))
+	if err != nil {
+		return nil, fmt.Errorf("requesting closest peers: %s\n", err)
 	}
 
+	peers := make([]string, 0)
 	decoder := json.NewDecoder(resp.Body)
+	defer resp.Body.Close()
 	if err = decoder.Decode(&peers); err != nil {
-		return nil, fmt.Errorf("decoding: %s\n", err)
+		return nil, fmt.Errorf("decoding closest peers: %s\n", err)
 	}
 	return peers, nil
+}
+
+// GetPeers retrieves the list of immediate peers of the API node
+func (c *Client) GetPeers() ([]string, error) {
+	resp, err := c.doRequest("peers")
+	if err != nil {
+		return nil, fmt.Errorf("requesting peers: %s\n", err)
+	}
+
+	peers := make([]string, 0)
+	decoder := json.NewDecoder(resp.Body)
+	defer resp.Body.Close()
+	if err = decoder.Decode(&peers); err != nil {
+		return nil, fmt.Errorf("decoding peers: %s\n", err)
+	}
+	return peers, nil
+}
+
+// GetListingsCount retrieves the list of contracts offered by the node
+// referenced by the hash originHash
+func (c *Client) GetListingsCount(originHash string) (int, error) {
+	resp, err := c.doRequest(fmt.Sprintf("listings/%s", originHash))
+	if err != nil {
+		return 0, fmt.Errorf("requesting listings: %s\n", err)
+	}
+
+	listings := make([]struct{}, 0)
+	decoder := json.NewDecoder(resp.Body)
+	defer resp.Body.Close()
+	if err = decoder.Decode(&listings); err != nil {
+		return 0, fmt.Errorf("decoding listings: %s\n", err)
+	}
+	return len(listings), nil
 }
---
+++ src/api/api_test.go
@@ -0,0 +1,172 @@
+package api_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/placer14/ob-crawler/api"
+	"gopkg.in/jarcoal/httpmock.v1"
+)
+
+func newTestClient() *api.Client {
+	return &api.Client{
+		Client:     &http.Client{},
+		Host:       "foo",
+		Port:       123,
+		AuthCookie: "bar",
+	}
+}
+
+func equalHashSlices(s1, s2 []string) bool {
+	if s1 == nil && s2 == nil {
+		return true
+	}
+	if s1 == nil || s2 == nil {
+		return false
+	}
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func setup() {
+	httpmock.Activate()
+}
+
+func teardown() {
+	httpmock.DeactivateAndReset()
+}
+
+func registerCookieValidatingResponder(url, expectedCookie string, response *http.Response, t *testing.T) {
+	var cookieMatched = false
+	httpmock.RegisterResponder(http.MethodGet, url,
+		func(req *http.Request) (*http.Response, error) {
+			for _, cookieValue := range req.Header["Cookie"] {
+				if expectedCookie == cookieValue {
+					cookieMatched = true
+				}
+			}
+			if cookieMatched == false {
+				t.Errorf("Expected cookie to be included, but was not\n\tActual headers: %+v", req.Header)
+			}
+			return response, nil
+		},
+	)
+}
+
+func TestGetPeersIsRequestedAndProcessedSuccessfully(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var (
+		c             = newTestClient()
+		testPath      = fmt.Sprintf("http://%s:%d/ob/peers", c.Host, c.Port)
+		expectedPeers = []string{
+			"QmRDcEDK9gSViAevCHiE6ghkaBCU7rTuQj4BDpmCzRvRYg",
+			"QmUZRGLhcKXF1JyuaHgKm23LvqcoMYwtb9jmh8CkP4og3K",
+		}
+	)
+
+	response, err := httpmock.NewJsonResponse(http.StatusOK, expectedPeers)
+	if err != nil {
+		t.Error(err)
+	}
+	registerCookieValidatingResponder(testPath, c.AuthCookie, response, t)
+
+	actualPeers, err := c.GetPeers()
+	if err != nil {
+		t.Error(err)
+	}
+	if equalHashSlices(expectedPeers, actualPeers) != true {
+		t.Errorf("Expected responses to match\n\tExpected: %+v\n\tActual: %+v\n", expectedPeers, actualPeers)
+	}
+}
+
+func TestGetClosestPeersIsRequestedAndProcessedSuccessfully(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var (
+		c             = newTestClient()
+		targetPeer    = "peer"
+		testPath      = fmt.Sprintf("http://%s:%d/ob/closestpeers/%s", c.Host, c.Port, targetPeer)
+		expectedPeers = []string{
+			"QmRDcEDK9gSViAevCHiE6ghkaBCU7rTuQj4BDpmCzRvRYg",
+			"QmUZRGLhcKXF1JyuaHgKm23LvqcoMYwtb9jmh8CkP4og3K",
+		}
+	)
+
+	response, err := httpmock.NewJsonResponse(http.StatusOK, expectedPeers)
+	if err != nil {
+		t.Error(err)
+	}
+	registerCookieValidatingResponder(testPath, c.AuthCookie, response, t)
+
+	actualPeers, err := c.GetClosestPeers(targetPeer)
+	if err != nil {
+		t.Error(err)
+	}
+	if equalHashSlices(expectedPeers, actualPeers) != true {
+		t.Errorf("Expected responses to match\n\tExpected: %+v\n\tActual: %+v\n", expectedPeers, actualPeers)
+	}
+}
+
+func TestGetListingsCountIsRequestedAndProcessedSuccessfully(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var (
+		c          = newTestClient()
+		targetPeer = "peer"
+		testPath   = fmt.Sprintf("http://%s:%d/ob/listings/%s", c.Host, c.Port, targetPeer)
+		// a list of anonymous things
+		listings      = []interface{}{struct{}{}, struct{}{}, struct{}{}}
+		expectedCount = len(listings)
+	)
+
+	response, err := httpmock.NewJsonResponse(http.StatusOK, listings)
+	if err != nil {
+		t.Error(err)
+	}
+	registerCookieValidatingResponder(testPath, c.AuthCookie, response, t)
+
+	count, err := c.GetListingsCount(targetPeer)
+	if err != nil {
+		t.Error(err)
+	}
+	if count != expectedCount {
+		t.Error("Expected response to be properly counted, but was not")
+	}
+}
+
+func TestGetListingsCountReturnsZeroWhenListingsNotFound(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var (
+		c          = newTestClient()
+		targetPeer = "peer"
+		testPath   = fmt.Sprintf("http://%s:%d/ob/listings/%s", c.Host, c.Port, targetPeer)
+	)
+
+	response, err := httpmock.NewJsonResponse(http.StatusNotFound, []interface{}{})
+	if err != nil {
+		t.Error(err)
+	}
+	registerCookieValidatingResponder(testPath, c.AuthCookie, response, t)
+
+	count, err := c.GetListingsCount(targetPeer)
+	if err == nil {
+		t.Error("Expected an error, where there was actually none")
+	}
+	if count != 0 {
+		t.Error("Expected response to be zero, but was not")
+	}
+}
